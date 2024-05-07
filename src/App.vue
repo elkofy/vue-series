146c4a7e1d2e69bd9b6ec0a970ea9c01a3462acf
Nhasbeen <46243642+elkofy@users.noreply.github.com>
2024-05-07 11:05:17 +0200

    :sparkles: simple implementation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <script setup>
+import Treeview from "./components/Treeview.vue";
+import {ref} from "vue";
+const fileStructure = {
+  name: "src",
+  type: "folder",
+  children: [
+    {
+      name: "components",
+      type: "folder",
+      children: [
+        {
+          name: "TreeItem.vue",
+          type: "file",
+        },
+        {
+          name: "Treeview.vue",
+          type: "file",
+        },
+      ],
+    },
+    {
+      name: "App.vue",
+      type: "file",
+    },
+  ],
+};
+const selectedFiles = ref([]);
 </script>
 
 <template>
   <h1>Vue Series</h1>
+  {{ selectedFiles }}
+  <Treeview :file-structure="fileStructure"  v-model="selectedFiles"/>
 </template>
 
 <style scoped>
